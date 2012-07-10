c95d4aa147098d47c376b1d416e12af9b50a3782
Patrick Moore <patmoore@amplafi.com>
2012-07-09 18:38:02 -0700

    correct license

--- src/main/java/org/amplafi/tapestry4/ProgrammableTapestryGlobals.java
+++ src/main/java/org/amplafi/tapestry4/ProgrammableTapestryGlobals.java
@@ -1,6 +1,15 @@
 /*
- * Created on Jun 23, 2007
- * Copyright 2006-2008 by Amplafi
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+ * the specific language governing permissions and limitations under the
+ * License.
  */
 package org.amplafi.tapestry4;
 
@@ -154,6 +163,7 @@ public class ProgrammableTapestryGlobals {
     public Iterator<Object> mocksIterator() {
         return new FilterIterator(requestObjects().iterator(),
                 new Predicate() {
+                    @Override
                     public boolean evaluate(Object object) {
                         return isMockObject(object);
                     }
